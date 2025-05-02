b80044802c0f446667dbe3c4d4b86b2783684c9a
kiltonpisli <kiltonpisli@gmail.com>
2025-05-02 23:51:25 +0200

    init commit

---
+++ strapi/src/api/tender/controllers/tender.ts
@@ -0,0 +1,7 @@
+/**
+ * tender controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tender.tender');
---
+++ strapi/src/api/vendor/routes/vendor.ts
@@ -0,0 +1,7 @@
+/**
+ * vendor router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::vendor.vendor');
